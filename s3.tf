495ecf49902501b85fa28aca158a523bcb9f7e61
Arian van Putten <arian.vanputten@gmail.com>
2023-08-17 19:04:46 +0000

    lol

--- s3.tf
+++ s3.tf
@@ -1,11 +1,3 @@
 resource "aws_s3_bucket" "cache" {
   bucket_prefix = "nixos-village-cache"
 }
-
-resource "aws_secretmanager_secret" "signing_key" {
-  name = "nixos-village-signing-key"
-}
-
-resource "aws_s3_bucket" "terraform" {
-  bucket_prefix = "nixos-village-terraform"
-}
