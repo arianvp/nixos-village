7c5a728d31d68fcb385b7cafbc74e7dc3ffb4ae4
Arian van Putten <arian.vanputten@gmail.com>
2023-08-17 17:07:32 +0000

    ssm

--- ec2.tf
+++ ec2.tf
@@ -9,10 +9,13 @@ resource "aws_key_pair" "admin" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCcJgoVsO3GT9aUVUZPTQrOydp+DwVagYlE3aEaslLFaIO65R+kit12mYSQ5J7tq7oDaAr9k09h4yl7onJsn16nO4RDoIAds6JjzdK6p9mjlHw2Kn570B3EnttPQk58tGj1936nXO5Vw/vLDzCgpYcCnfGrCBP1C3MoMnZ3Z51zlogSOMSz7DFmQNCDilnhup2cXmC8ORjg2l+WbkROyNpkS5ZXEjtciJ+o41LkyYjwyDnO60zTRKCu3q2eEht/+eCC859EiYelehUQV9qIIOaUnHtMhO5eUoJLGbsTqzknrHDj0Ff+oJPZqIP0SLk9TE1LoSZkZotx0C4L3f/dvqecPtfuagxE5K9TLEa0427/qQxnFvC4rlur3GjoF3EyaXDMdiN8a0/WhkXkDvGuu7RG2FjDy4sSwWAyO7djmRGq+z7lb+lDEjruiyBqGO71Ay7+sOvGiBCWvUI4zMvp3qQf6Yc9Y5YDRfUJ/a9AXQMLsWmiERMunAITWHipHKYgd7U= arian@framework"
 }
 
-resource "aws_launch_template" "nixos" {
+resource "aws_launch_template" "webserver" {
   image_id      = local.nixos_ami
   instance_type = local.instance_type
   key_name      = aws_key_pair.admin.key_name
+  iam_instance_profile {
+    name = aws_iam_instance_profile.webserver.name
+  }
 
   /*
   Sets the user data to the nix config.
@@ -38,15 +41,15 @@ resource "aws_launch_template" "nixos" {
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name = "nixos"
+      Name = "webserver"
     }
   }
 }
 
-resource "aws_instance" "nixos" {
+resource "aws_instance" "webserver" {
   launch_template {
-    name    = aws_launch_template.nixos.name
-    version = aws_launch_template.nixos.latest_version
+    name    = aws_launch_template.webserver.name
+    version = aws_launch_template.webserver.latest_version
   }
-  tags = { Name = "nixos" }
+  tags = { Name = "webserver" }
 }
