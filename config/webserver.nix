250a861973cbf74b77578f896c9cbf71f414d352
Arian van Putten <arian.vanputten@gmail.com>
2023-08-18 10:44:30 +0000

    complete lifecycle

--- config/webserver.nix
+++ config/webserver.nix
@@ -15,39 +15,7 @@
     wantedBy = [ "multi-user.target" ];
     after = [ "nginx.service" ];
     path = [ pkgs.awscli2 pkgs.curl ];
-    script = ''
-      function get_target_state {
-        curl -sSf http://169.254.169.254/latest/meta-data/autoscaling/target-lifecycle-state
-      }
-
-      function complete_lifecycle_action {
-        instance_id=$(curl -sSf http://169.254.169.254/latest/meta-data/instance-id)
-        group_name=$(curl -sSf http://169.254.169.254/latest/meta-data/tags/instance/aws:autoscaling:groupName)
-        region=$(curl -sSf http://169.254.169.254/latest/meta-data/placement/region)
-  
-        aws autoscaling complete-lifecycle-action \
-          --lifecycle-hook-name launching \
-          --auto-scaling-group-name $group_name \
-          --lifecycle-action-result CONTINUE \
-          --instance-id $instance_id \
-          --region $region
-      }
-
-      function main {
-          while true
-          do
-              target_state=$(get_target_state)
-              if [ \"$target_state\" = \"InService\" ]; then
-                  complete_lifecycle_action
-                  break
-              fi
-              echo $target_state
-              sleep 5
-          done
-      }
-
-      main
-    '';
+    script = builtins.readFile ./complete-lifecycle-action.sh;
 
   };
   system.stateVersion = "23.05";
