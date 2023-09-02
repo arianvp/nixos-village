610848ad7f0a66917689703f818575caba1b1856
Arian van Putten <arian.vanputten@gmail.com>
2023-09-02 10:16:53 +0000

    fmt

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    key    = "terraform.tfstate"
+    key = "terraform.tfstate"
   }
 }
 
--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,3 @@
 module "vpc" {
-    source = "./modules/vpc"
+  source = "./modules/vpc"
 }
