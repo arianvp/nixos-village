82f33bd46406fc98da9debb2eff9974cc23bf51f
Arian van Putten <arian.vanputten@gmail.com>
2024-03-17 10:30:14 +0100

    pragmatism

--- deploy/provider.tf
+++ deploy/provider.tf
@@ -1,13 +1,11 @@
 terraform {
   backend "s3" {
+    bucket = "arn:aws:s3:::nixos-village-terraform20240316094340583200000001"
+    region = "eu-central-1"
     key = "terraform.tfstate"
   }
 }
 
 provider "aws" {
-  region = "eu-central-1"
-}
-
-provider "awscc" {
   region = "eu-central-1"
 }
