01f7a3adebdb8fcba24ac8a6205f9617fd20bc26
Arian van Putten <arian.vanputten@gmail.com>
2024-05-31 22:08:26 +0200

    No fluent-bit

--- nix/configs/web.nix
+++ nix/configs/web.nix
@@ -1,8 +1,6 @@
 { pkgs, modulesPath, ... }:
 {
   imports = [
-    ../modules/fluent-bit.nix
-    ../mixins/aws.nix
     (modulesPath + "/virtualisation/amazon-image.nix")
   ];
 
@@ -11,12 +9,6 @@
     enable = true;
   };
 
-  services.fluent-bit = {
-    enable = true;
-    settings.pipeline = {
-      inputs = [{ name = "systemd"; }];
-    };
-  };
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   system.stateVersion = "24.05";
 
