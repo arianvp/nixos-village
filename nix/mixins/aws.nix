829914c97aa6969c917b2fd2a2f490d45ce7f6ad
Arian van Putten <arian.vanputten@gmail.com>
2024-06-01 15:16:23 +0200

    Fix accessor syntax

--- nix/mixins/aws.nix
+++ nix/mixins/aws.nix
@@ -9,7 +9,7 @@
         log_group_name = "ec2-logs";
         auto_create_group = true;
         log_stream_name = "default";
-        log_stream_template = "\${ec2_instance_id}";
+        log_stream_template = "\$ec2_instance_id";
       }];
     };
   };
