5b5335b3f22d1b75816034c5328e3879965fc831
Arian van Putten <arian.vanputten@gmail.com>
2024-06-01 09:46:59 +0200

    Add rollback

--- deploy/modules/ssm_documents/deploy.tf
+++ deploy/modules/ssm_documents/deploy.tf
@@ -1,6 +1,25 @@
-locals {
-  command = file("${path.module}/deploy.sh")
+resource "aws_ssm_document" "nixos_rollback" {
+  name          = "NixOS-Rollback"
+  document_type = "Command"
+  content = jsonencode({
+    schemaVersion = "2.2"
+    description   = "Rollback NixOS"
+    parameters = {
+      profile = {
+        type    = "String"
+        default = "/nix/var/nix/profiles/system"
+      }
+    }
+    mainSteps = [
+      {
+        action = "aws:runShellScript"
+        name   = "rollback"
+        inputs = { runCommand = [file("${path.module}/rollback.sh")] }
+      }
+    ]
+  }) 
 }
+
 resource "aws_ssm_document" "nixos_deploy" {
   name          = "NixOS-Deploy"
   document_type = "Command"
