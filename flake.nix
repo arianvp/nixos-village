5abc813e78e5b9812f72289d2b821a3309b980d5
Arian van Putten <arian.vanputten@gmail.com>
2024-07-20 22:45:50 +0200

    try namespace labs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,33 @@
 {
   description = "NixOS Village AWS cloud";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-  inputs.nixpkgs-amazon-ssm-agent.url = "github:r-ryantm/nixpkgs?ref=auto-update/amazon-ssm-agent";
-  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    nix-github-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = inputs@{ self, nixpkgs, pre-commit-hooks, ... }: {
+  outputs = inputs@{ self, nixpkgs, pre-commit-hooks, nix-github-actions, ... }: {
     lib.supportedSystems = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ];
     lib.forAllSystems = nixpkgs.lib.genAttrs self.lib.supportedSystems;
+
+    githubActions = nix-github-actions.lib.mkGithubMatrix {
+      inherit (self) checks;
+      platforms = {
+        x86_64-linux = [
+            "nscloud-ubuntu-22.04-amd64-4x16-with-cache"
+            "nscloud-cache-size-20gb"
+            "nscloud-cache-tag-aarch64-linux"
+        ];
+        aarch64-linux = [
+            "nscloud-ubuntu-22.04-arm64-4x16-with-cache"
+            "nscloud-cache-size-20gb"
+            "nscloud-cache-tag-x86_64-linux"
+        ];
+      };
+    };
 
     devShells = self.lib.forAllSystems (system: {
       default = with nixpkgs.legacyPackages.${system}; mkShell {
